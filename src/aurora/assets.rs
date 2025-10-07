788a5647749b8ebc0774d7e6748934dc27059ec5
jrobiche <33152499+jrobiche@users.noreply.github.com>
2025-10-06 19:48:08 -0500

    move asset related code from utils to aurora::assets

--- src/aurora/assets.rs
+++ src/aurora/assets.rs
@@ -24,10 +24,7 @@ pub use image;
 use std::path::Path;
 
 use self::AssetType::*;
-use crate::utils::{
-    apply_endian, apply_swizzle, create_parent_directories, GenericResult, TextureEndian,
-    TextureFormat,
-};
+use crate::utils::{create_parent_directories, GenericResult};
 
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
 #[repr(usize)]
@@ -194,6 +191,122 @@ impl AssetType {
         ]
         .iter()
         .copied()
+    }
+
+    pub fn to_u32(&self) -> u32 {
+        *self as u32
+    }
+
+    pub fn to_usize(&self) -> usize {
+        *self as usize
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+pub enum TextureEndian {
+    EndianNone = 0,
+    Endian8in16 = 1,
+    Endian8in32 = 2,
+    Endian16in32 = 3,
+}
+
+impl TextureEndian {
+    pub fn display(&self) -> &str {
+        match self {
+            TextureEndian::EndianNone => "TextureEndian::EndianNone",
+            TextureEndian::Endian8in16 => "TextureEndian::Endian8in16",
+            TextureEndian::Endian8in32 => "TextureEndian::Endian8in32",
+            TextureEndian::Endian16in32 => "TextureEndian::Endian16in32",
+        }
+    }
+
+    pub fn from_u32(value: u32) -> GenericResult<TextureEndian> {
+        let endian = match value {
+            0 => Self::EndianNone,
+            1 => Self::Endian8in16,
+            2 => Self::Endian8in32,
+            3 => Self::Endian16in32,
+            _ => {
+                let msg = format!("Could not convert value '{}' to TextureEndian.", value);
+                return Err(msg.into());
+            }
+        };
+        Ok(endian)
+    }
+
+    pub fn from_usize(value: usize) -> GenericResult<TextureEndian> {
+        let endian = match value {
+            0 => Self::EndianNone,
+            1 => Self::Endian8in16,
+            2 => Self::Endian8in32,
+            3 => Self::Endian16in32,
+            _ => {
+                let msg = format!("Could not convert value '{}' to TextureEndian.", value);
+                return Err(msg.into());
+            }
+        };
+        Ok(endian)
+    }
+
+    pub fn to_u32(&self) -> u32 {
+        *self as u32
+    }
+
+    pub fn to_usize(&self) -> usize {
+        *self as usize
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+pub enum TextureFormat {
+    RGBA8 = 6,
+    BC3 = 20,
+}
+
+impl TextureFormat {
+    pub fn bytes_per_pixel(&self) -> usize {
+        match self {
+            TextureFormat::RGBA8 => 4,
+            TextureFormat::BC3 => 1,
+        }
+    }
+
+    pub fn bytes_per_pixel_u32(&self) -> u32 {
+        match self {
+            TextureFormat::RGBA8 => 4 as u32,
+            TextureFormat::BC3 => 1 as u32,
+        }
+    }
+
+    pub fn display(&self) -> &str {
+        match self {
+            TextureFormat::RGBA8 => "TextureFormat::RGBA8",
+            TextureFormat::BC3 => "TextureFormat::BC3",
+        }
+    }
+
+    pub fn from_u32(value: u32) -> GenericResult<TextureFormat> {
+        let endian = match value {
+            6 => Self::RGBA8,
+            20 => Self::BC3,
+            _ => {
+                let msg = format!("Could not convert value '{}' to TextureFormat.", value);
+                return Err(msg.into());
+            }
+        };
+        Ok(endian)
+    }
+
+    pub fn from_usize(value: usize) -> GenericResult<TextureFormat> {
+        let endian = match value {
+            6 => Self::RGBA8,
+            20 => Self::BC3,
+            _ => {
+                let msg = format!("Could not convert value '{}' to TextureFormat.", value);
+                return Err(msg.into());
+            }
+        };
+        Ok(endian)
     }
 
     pub fn to_u32(&self) -> u32 {
@@ -1569,5 +1682,51 @@ impl GPUTextureFetch {
 
     pub fn set_border_color(&mut self, value: u32) -> () {
         self.constant5 = (self.constant5 & !0x00000003) | (value & 0b11)
+    }
+}
+
+pub fn apply_endian(texture_endian: TextureEndian, buffer: &mut Vec<u8>) -> () {
+    match texture_endian {
+        TextureEndian::EndianNone => (),
+        TextureEndian::Endian8in16 => {
+            for i in (0..buffer.len()).step_by(2) {
+                buffer.swap(i, i + 1);
+            }
+        }
+        TextureEndian::Endian8in32 => {
+            for i in (0..buffer.len()).step_by(4) {
+                buffer.swap(i, i + 3);
+                buffer.swap(i + 1, i + 2);
+            }
+        }
+        TextureEndian::Endian16in32 => {
+            for i in (0..buffer.len()).step_by(4) {
+                buffer.swap(i, i + 2);
+                buffer.swap(i + 1, i + 3);
+            }
+        }
+    }
+}
+
+pub fn apply_swizzle(
+    buffer: &mut Vec<u8>,
+    swizzle_x: usize,
+    swizzle_y: usize,
+    swizzle_z: usize,
+    swizzle_w: usize,
+) -> () {
+    let mut x: u8;
+    let mut y: u8;
+    let mut z: u8;
+    let mut w: u8;
+    for i in (0..buffer.len()).step_by(4) {
+        x = buffer[i + swizzle_x];
+        y = buffer[i + swizzle_y];
+        z = buffer[i + swizzle_z];
+        w = buffer[i + swizzle_w];
+        buffer[i] = x;
+        buffer[i + 1] = y;
+        buffer[i + 2] = z;
+        buffer[i + 3] = w;
     }
 }
